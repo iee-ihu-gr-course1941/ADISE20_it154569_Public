e42c6a3cce06cac8db16c9eea072e96de67620d2
Ilias Chorafas <i.chorafas.it@gmail.com>
2020-12-23 21:52:44 +0200

    log_in kai dbconnect

---
+++ db_config.php
@@ -0,0 +1,20 @@
+<?php
+$host = "127.0.0.1";
+$db = "ADISE2020_db";
+
+$user = "root";
+$pass = "19977991AnyonE!";
+
+
+if (gethostname() == 'users.iee.ihu.gr') {
+    $mysqli = new mysqli($host, $user, $pass, $db, null, '/home/student/it/2015/it154569/mysql/run/mysql.sock');
+} else {
+    $mysqli = new mysqli($host, $user, $pass, $db, "3333");
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" .
+        $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+$GLOBALS["mysqli"] = $mysqli;
