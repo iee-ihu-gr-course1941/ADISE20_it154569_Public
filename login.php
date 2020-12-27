85f08194ce5ee81fad5136013068cfff9b35b17d
Ilias C <67147533+IliasChorafas97@users.noreply.github.com>
2020-12-27 19:05:04 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,54 +1,155 @@
 <?php
 require_once "db_config.php";
 
-
 global $mysqli;
 
-if(isset($_REQUEST["username"])){
-    $username = $_REQUEST["username"];
 
+if (isset($_REQUEST["nickname"]) && isset($_REQUEST["color"])) {
+    $nickname = $_REQUEST["nickname"];
+    $color = $_REQUEST["color"];
     $token = uniqid();
-    $time = time();
 
 
+    $activePlayersCounts = getActivePlayerCount();
+    $isColorValid = checkColorValid($color);
+
+    if ($isColorValid) {
+        if ($activePlayersCounts == 0) {
+            // prepare and bind
+            $stmt = $mysqli->prepare("insert into players (nickname, pawn_color, token) VALUES (?, ?, ?)");
+            // set parameters and execute
+            $stmt->bind_param("sss", $nickname, $color, $token);
+            $stmt->execute();
+
+
+            $game_status = "initialized";
+            $sql = "update game_status set status=?, p_turn=?";
+            $st = $mysqli->prepare($sql);
+            $st->bind_param("ss", $game_status, $color);
+            $st->execute();
+
+
+            header('Content-type: application/json');
+            echo json_encode(array("nickname" => $nickname, "token" => $token, "color" => $color));
+
+        } elseif ($activePlayersCounts == 1) {
+            $isColorTaken = checkTakenColor($color);
+            $isNicknameTaken = checkTakenNickname($nickname);
+
+            if (!$isColorTaken && !$isNicknameTaken) {
+                // prepare and bind
+                $stmt = $mysqli->prepare("insert into players (nickname, pawn_color, token) VALUES (?, ?, ?)");
+                // set parameters and execute
+                $stmt->bind_param("sss", $nickname, $color, $token);
+                $stmt->execute();
+
+
+                $game_status = "started";
+                $sql = "update game_status set status=?";
+                $st = $mysqli->prepare($sql);
+                $st->bind_param("s", $game_status);
+                $st->execute();
+
+
+
+                header('Content-type: application/json');
+                echo json_encode(array("nickname" => $nickname, "token" => $token, "color" => $color));
+
+            } else {
+                http_response_code(400);
+                header('Content-type: application/json');
+                if ($isColorTaken) {
+                    echo json_encode(array("error" => "Color is taken"));
+                } else {
+                    echo json_encode(array("error" => "Nickname is taken"));
+                }
+            }
+        } else {
+            http_response_code(400);
+            header('Content-type: application/json');
+            echo json_encode(array("error" => "Maximum 2 players allowed. Wait for them to finish."));
+        }
+
+    } else {
+        http_response_code(400);
+        header('Content-type: application/json');
+        echo json_encode(array("error" => "Color is invalid"));
+    }
+} else {
+    http_response_code(400);
+    header('Content-type: application/json');
+    if (!isset($_REQUEST["nickname"])) {
+        echo json_encode(array("error" => "No nickname given"));
+    } elseif (!isset($_REQUEST["color"])) {
+        echo json_encode(array("error" => "No color given"));
+    }
+}
+
+
+function getActivePlayerCount()
+{
+    global $mysqli;
+    $activePlayers = -1;
 
     $checkActivePlayersSQL = "select count(*) from players";
 
     $result = $mysqli->query($checkActivePlayersSQL);
-    $activePlayers = -1;
 
     if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
             $activePlayers = $row["count(*)"];
         }
     } else {
         $activePlayers = 0;
     }
 
-
-    if($activePlayers==0){
-        $insertFirstPlayerSQL = "INSERT INTO players (nickname, pawn_color, token, last_change) VALUES ('$username', 'B', '$token', null)";
-
-        if (mysqli_query($mysqli, $insertFirstPlayerSQL)) {
-            echo json_encode(array("token"=>$token));
-        } else {
-            echo "Error: " . $insertFirstPlayerSQL . "<br>" . mysqli_error($mysqli);
-        }
-    }
-    elseif ($activePlayers==1){
-        $insertSecondPlayerSQL = "INSERT INTO players (nickname, pawn_color, token, last_change) VALUES ('$username', 'Y', '$token', null)";
-
-        if (mysqli_query($mysqli, $insertSecondPlayerSQL)) {
-            echo json_encode(array("token"=>$token));
-        } else {
-            echo "Error: " . $insertSecondPlayerSQL . "<br>" . mysqli_error($mysqli);
-        }
-    }
-    else{
-        echo json_encode(array("error"=>"Maximum 2 players allowed. Wait for them to finish."));
-    }
-}else{
-    echo json_encode(array("error"=>"No username given"));
+    return $activePlayers;
 }
+
+
+function checkColorValid($color)
+{
+    if ($color != "B" && $color != "b" && $color != "Y" && $color != "y") {
+        return false;
+    }
+    return true;
+}
+
+
+function checkTakenColor($color)
+{
+    global $mysqli;
+    $stmt = $mysqli->prepare("SELECT * FROM players WHERE pawn_color = ? ");
+    $stmt->bind_param("s", $color);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $result_count = $result->num_rows;
+
+    if ($result_count > 0) {
+        return true;
+    }
+    return false;
+}
+
+
+function checkTakenNickname($nickname)
+{
+    global $mysqli;
+    $stmt = $mysqli->prepare("SELECT * FROM players WHERE nickname = ? ");
+    $stmt->bind_param("s", $nickname);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $result_count = $result->num_rows;
+
+    if ($result_count > 0) {
+        return true;
+    }
+    return false;
+}
+
+
+
+
+
 
 
